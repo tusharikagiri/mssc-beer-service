5e6ebd513b25dbd163bdaf5c9fd25db47a543307
tusharikagiri <126240171+tusharikagiri@users.noreply.github.com>
2025-01-03 10:27:13 +0530

    Fixed issue of mapper bean creation conflict

--- src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 
-@Component
 @Mapper(componentModel = "spring", uses = {DateMapper.class})
 public interface BeerMapper {
 	
